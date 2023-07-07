2c2bd9c9371183f2fce31c0eff9e4c0fc0fcbb07
Pravyn-P <pravyn.it@gmail.com>
2023-07-07 16:06:31 +0530

    msg

--- session-4/data.tf
+++ session-4/data.tf
@@ -1,6 +1,6 @@
 data "aws_ami" "ami-info" {
     most_recent = true
-    owners = ["137112412989"] # this is the amazon's account id itself
+    owners = [var.aws_accounts[data.aws_region.current.name]] # this is the amazon's account id itself
     filter {
     name = "name"
     values = ["amzn2-ami-kernel-*-x86_64-gp2"]
@@ -13,4 +13,11 @@ data "aws_ami" "ami-info" {
     name = "virtualization-type"
     values = ["hvm"]
     }
+}
+
+# I want to fetch which region i am in. The below example will fetch the info regarding your current region
+data "aws_region" "current" {}
+
+output "current-region" {
+    value = data.aws_region.current.name
 }
